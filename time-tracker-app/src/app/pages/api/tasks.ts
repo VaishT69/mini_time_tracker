7fc055e472c859e6fbb6eb1361c725fee3bf041a
vaishT09 <164990823+vaishT09@users.noreply.github.com>
2025-06-21 16:31:28 +0200

    git -m "Installed axios and zustand, added zustand store and api module"

---
+++ time-tracker-app/src/app/pages/api/tasks.ts
@@ -0,0 +1,29 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+let tasks: {taskName: string; hoursWorked: number } [] = [];
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method === 'GET') {
+        return res.status(200).json(tasks);
+    }
+
+    if (req.method === 'POST') {
+        const {taskName, hoursWorked} = req.body; 
+        if (!taskName || typeof hoursWorked !== 'number') {
+            return res.status(400).json({ error: "Invalid input"});
+        }
+        const newTask = {taskName, hoursWorked};
+        tasks.push(newTask);
+        return res.status(201).json(newTask);
+    }
+
+    if (req.method === 'DELETE') {
+        const {index} = req.body;
+        if (typeof index !== 'number' || index < 0 || index >= tasks.length) {
+            return res.status(400).json({error: 'Invalid Index'});
+        }
+        tasks.splice(index, 1);
+        return res.status(200).json({success: true});
+    }
+    return res.status(405).json({error: 'Method not allowed'});
+ }
---
+++ time-tracker-app/src/app/store/taskStore.ts
@@ -0,0 +1,35 @@
+import { create } from "zustand";
+import axios from "axios";
+
+type Task ={
+    taskName: string;
+    hoursWorked: number;
+};
+
+type TaskStore = {
+    tasks: Task[];
+    fetchTasks: () => Promise<void>;
+    addTask: (task: Task) => Promise<void>;
+    deleteTask: (index: number) => Promise<void>;
+};
+
+export const useTaskStore = create<TaskStore>((set) => ({
+    tasks: [],
+
+    fetchTasks: async () => {
+        const res = await axios.get('/api/tasks');
+        set({ tasks: res.data});
+    },
+
+    addTask: async (task) => {
+        await axios.post('/api/tasks', task);
+        const res = await axios.get('/api/tasks');
+        set({ tasks: res.data});
+    },
+
+    deleteTask: async (index) => {
+        await axios.delete('/api/tasks', {data: {index}});
+        const res = await axios.get('/api/tasks');
+        set({tasks: res.data});
+    },
+}));
