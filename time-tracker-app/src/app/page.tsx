a39d18965c043a2cca8b969a200321aefcc1cdfc
vaishT09 <164990823+vaishT09@users.noreply.github.com>
2025-06-22 16:49:05 +0200

    git -m "Added heading"

--- time-tracker-app/src/app/page.tsx
+++ time-tracker-app/src/app/page.tsx
@@ -6,7 +6,11 @@ import TaskTimercomponent from "./components/TaskTimeComponent";
 export default function Home() {
   return (
     <div className="px-6 flex flex-col items-center justify-items-center min-h-screen p-8 pb-20 gap-10 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-    
+      <div className="mt-10">
+        <h1 className="text-white font-bold items-center text-4xl">
+           Time Tracker App
+        </h1>
+      </div>
       <div className="w-full md:w-1/2 lg:w-1/3 max-w-2xl">
         <TaskFormComponent />
       </div>
@@ -16,7 +20,6 @@ export default function Home() {
       <div className="w-full max-w-3xl">
         <TaskListComponent />
       </div>
-  
     </div>
   );
 }
